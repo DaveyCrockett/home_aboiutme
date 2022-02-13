0f4c000c6fed6e2e78240f0aa7406baab0e24496
DaveyCrockett <67625797+DaveyCrockett@users.noreply.github.com>
2022-02-13 00:52:55 -0800

    Delete pages directory

--- pages/views.py
+++
@@ -1,9 +0,0 @@
-# Create your views here.
-from re import template
-from django.views.generic import TemplateView
-
-class HomePageView(TemplateView):
-    template_name = "index.html"
-
-class AboutPageView(TemplateView):
-    template_name = "about.html"
