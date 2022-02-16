ee63706d338056f6fbed38b0cca651c53c0161fb
augustspies22 <augustspies22@gmail.com>
2022-02-15 16:40:43 -0800

    turn in

--- home_aboutme/config/settings.py
+++ home_aboutme/config/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-ousf1!7fyj84s$#oz+dgn)_49oym#oh$w#*r17j5zb5-da+am+
 DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com",
-                    "127.0.0.1"]
+                    "127.0.0.1",
+                    "localhost"]
 
 
 # Application definition
